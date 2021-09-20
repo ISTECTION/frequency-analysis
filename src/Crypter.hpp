6657e5a575c485fd597d2f7a2c108933d5f24a00
Владислав Глушко <perfectionist.one.love@otlook.com>
2021-09-21 01:29:36 +0700

    add FrequencyAnalysis

--- src/Crypter.hpp
+++ src/Crypter.hpp
@@ -1,15 +1,18 @@
 #ifndef _CRYPTER_H_
 #define _CRYPTER_H_
+#include <unordered_map>
+#include <filesystem>
 #include <iostream>
-#include <string>
+#include <numeric>
 #include <codecvt>
 #include <sstream>
 #include <fstream>
-#include <filesystem>
+#include <locale>
+#include <string>
+#include <vector>
 #include <chrono>
 #include <random>
 #include <regex>
-#include <locale>
 #include <map>
 
 #define _CRYPTER_BEGIN namespace crypter {
@@ -25,47 +28,45 @@
 _CRYPTER_BEGIN
 using uint = unsigned int;
 using SCS = std::chrono::system_clock;
+using pair_wchar = std::pair<wchar_t, uint>;
+
 
 const uint16_t COUNT_RUSSIAN_ABC = 33;
 
 //? const PATH ------------------------------------------------------ ///
-const _FILESYSTEM path ASSAY           = "texts/assay.txt";
-const _FILESYSTEM path ENCRYPTED_ASSAY = "texts/encrypted_assay.txt";
+const _FILESYSTEM path KEY = "output/key.txt";
+
+const _FILESYSTEM path ASSAY = "texts/assay.txt";
+const _FILESYSTEM path ASSAY_ENCRYPTED = "texts/assay_encrypted.txt";
 
 const _FILESYSTEM path COMPOSITION = "texts/composition.txt";
+const _FILESYSTEM path COMPOSITION_ENCRYPTED = "texts/composition_encrypted.txt";
 
-const _FILESYSTEM path KEY = "output/key.txt";
 const _FILESYSTEM path RUSSIAN_MONOGRAMS = "rus/russian_monograms.txt";
+const _FILESYSTEM path RUSSIAN_BIGRAMS = "rus/russian_bigrams.txt";
 //? const PATH ------------------------------------------------------ ///
+
+/// Index: 6 - Russian `E
+uint getIndexArray(const uint _Off) { return _Off < 1078 ? _Off - 1072 : _Off == 1105 ? 6 : _Off - 1071; }
 
 class Crypter
 {
 private:
-    _STD wstring _Key;
-
     void locale() const;
     uint checkLetter(const uint) const;
+    _STD wstring getLowerStr(_STD wstring) const;
     uint getLowerID_RU_UTF8(const uint) const;
 
-    void openFileUTF8(_STD wstring &_Key, const _FILESYSTEM path); //? delete
     _STD wstring formatingText(_STD wstring &_Text);
     void codeConvertUTF8(std::wfstream &_Stream);
 
     uint getSeed() const;
     uint getRandom(std::mt19937 &engine, uint, uint) const;
+
+    void inFileUTF8_FORMAT_WCHAR_COUNT(std::vector<std::pair<std::wstring, uint>> &_V, const _FILESYSTEM path);
+    double dispersion(std::unordered_map<std::wstring, uint> &_T, std::unordered_map<std::wstring, uint> &_F);
 public:
-    Crypter() { 
-        locale();
-        // std::wstring key = generateRandomKey();
-
-        std::wstring key = inKeyFromFileUTF8(KEY);
-    
-        std::wstring text = DecryptText(key, ENCRYPTED_ASSAY);
-        wcout_t(text);
-
-        // outFileUTF8(key, KEY);
-        // outFileUTF8(text, ENCRYPTED_ASSAY);
-    };
+    Crypter() { locale(); };
     ~Crypter() { };
 
     _STD wstring EncryptText(std::wstring& key, const _FILESYSTEM path);
@@ -76,11 +77,14 @@ public:
 
     _STD wstring inKeyFromFileUTF8 (const _FILESYSTEM path);
     void outFileUTF8 (_STD wstring &_Text, const _FILESYSTEM path);
+
+    void FrequencyAnalysis(const _FILESYSTEM path);
 };
 
 _STD wstring Crypter::EncryptText(std::wstring& key, const _FILESYSTEM path _Path) {
     std::wfstream fin(_Path, std::wfstream::in); 
-    if(!fin) std::cerr << "File was not open";
+    if(!fin) 
+        std::cerr << "File was not open";
 
     codeConvertUTF8(fin);
     std::wstring text, line;
@@ -93,7 +97,8 @@ _STD wstring Crypter::EncryptText(std::wstring& key, const _FILESYSTEM path _Pat
     std::map<wchar_t, wchar_t> dict;
 
     for_t (ABC.size()) dict[ABC[_Pos]] = key[_Pos];
-    for_t (formattedText.size()) 
+
+    for(size_t _Pos = 0; _Pos < formattedText.size(); _Pos++)
         formattedText[_Pos] = dict[formattedText[_Pos]];
 
     return formattedText;
@@ -101,7 +106,8 @@ _STD wstring Crypter::EncryptText(std::wstring& key, const _FILESYSTEM path _Pat
 
 _STD wstring Crypter::DecryptText(std::wstring& key, const _FILESYSTEM path _Path) {
     std::wfstream fin(_Path, std::wfstream::in); 
-    if(!fin) std::cerr << "File was not open";
+    if(!fin) 
+        std::cerr << "File was not open";
     codeConvertUTF8(fin);
     std::wstring text, line;
     while (std::getline(fin, line)) 
@@ -112,7 +118,8 @@ _STD wstring Crypter::DecryptText(std::wstring& key, const _FILESYSTEM path _Pat
     std::map<wchar_t, wchar_t> dict;
 
     for_t (ABC.size()) dict[key[_Pos]] = ABC[_Pos];
-    for_t (text.size()) 
+
+    for(size_t _Pos = 0; _Pos < text.size(); _Pos++)
         text[_Pos] = dict[text[_Pos]];
 
     return text;
@@ -150,7 +157,6 @@ _STD wstring Crypter::generateRandomKey() const {
 
 _STD wstring Crypter::getRussianABC() const {
     const size_t LETTER_A = 1072, INC_LETTER_E = 1078;
-
     std::wstring ABC;
     ABC.resize(COUNT_RUSSIAN_ABC);
 
@@ -174,14 +180,28 @@ uint Crypter::getLowerID_RU_UTF8(const uint _Id) const { return _Id == 1025 ? 11
 uint Crypter::getSeed() const { return SCS::now().time_since_epoch().count(); }
 void Crypter::locale() const { setlocale(LC_ALL, "Russian"); }  
 
-void Crypter::openFileUTF8(_STD wstring& _Key, const _FILESYSTEM path _Path) {
-    std::wfstream fin(_Path, std::wfstream::in); 
+_STD wstring Crypter::getLowerStr(_STD wstring _Str) const {
+    std::wstring lowerStr;
+    for(const auto &_Symbol : _Str)
+        lowerStr += getLowerID_RU_UTF8(_Symbol);
+    
+    return lowerStr;
+}
+
+void Crypter::inFileUTF8_FORMAT_WCHAR_COUNT(std::vector<std::pair<std::wstring, uint>> &_V,
+     const _FILESYSTEM path _Path) {
+
+    std::wfstream fin(_Path, std::wfstream::in);
+    if (!fin) 
+        std::cerr << "File was not open";
     codeConvertUTF8(fin);
-    for (std::wstring line; std::getline(fin, line);) {
+    std::wstring line;
+    while (std::getline(fin, line)) {
         std::wstringstream wstream(line);
-        wchar_t _Symbol;
-        wstream >> _Symbol;
-        _Key.push_back(getLowerID_RU_UTF8(static_cast<uint>(_Symbol)));
+        std::wstring _Str;
+        uint _Size;
+        wstream >> _Str >> _Size;
+        _V.push_back(std::pair(getLowerStr(_Str), _Size));
     }
     fin.close();
 }
@@ -205,6 +225,116 @@ void Crypter::outFileUTF8(_STD wstring& _Text, const _FILESYSTEM path _Path) {
 
 void Crypter::codeConvertUTF8(std::wfstream &_Stream) { 
     _Stream.imbue(std::locale(std::locale(), new std::codecvt_utf8<wchar_t>)); 
+}
+
+void Crypter::FrequencyAnalysis(const _FILESYSTEM path _Path) {
+    std::wfstream fin(_Path, std::wfstream::in); 
+    if(!fin) 
+        std::cerr << "File was not open";
+
+    codeConvertUTF8(fin);
+    std::wstring text, line;
+    while (std::getline(fin, line)) 
+        text += line;
+    fin.close();
+
+    std::vector<std::pair<wchar_t, uint>> countMonograms;
+    countMonograms.resize(COUNT_RUSSIAN_ABC);
+
+    std::wstring ABC = getRussianABC();
+    for(size_t _Pos = 0; _Pos < ABC.size(); _Pos++) {
+        countMonograms[_Pos].first = ABC[_Pos];
+        countMonograms[_Pos].second = ZERO;
+    }
+
+    for (const auto &_El : text) 
+        countMonograms[getIndexArray(_El)].second++;
+    
+    uint countAllSymbol = std::accumulate(countMonograms.begin(), countMonograms.end(), 0, 
+        [](const std::size_t _S, const auto &_Elem) { return _S + _Elem.second; });
+
+    std::sort(countMonograms.begin(), countMonograms.end(), 
+        [] (const pair_wchar &_Left, const pair_wchar &_Right) { return _Left.second > _Right.second; });
+
+    std::wstring monogramsKey;
+    std::wostringstream w_stream;
+    for(auto &_El : countMonograms)
+        w_stream << _El.first;
+    monogramsKey = w_stream.str();
+
+    std::vector<std::pair<std::wstring, uint>> monogramsFile;
+    inFileUTF8_FORMAT_WCHAR_COUNT(monogramsFile, RUSSIAN_MONOGRAMS);
+
+    std::unordered_map<wchar_t, wchar_t> monogramsMap;
+    for (size_t _Pos = 0; _Pos < monogramsKey.size(); _Pos++)
+        monogramsMap[monogramsKey[_Pos]] = monogramsFile[_Pos].first.at(0);
+
+    std::wstring textDecrypted;
+    for (auto &x : text) 
+        textDecrypted += monogramsMap[x];
+
+    std::unordered_map<std::wstring, uint> bigramsText;
+    for (int _Pos = 0; _Pos < textDecrypted.size() - 1; _Pos++) {
+        std::wstring currentBigram = textDecrypted.substr(_Pos, 2);
+
+        if(bigramsText.find(currentBigram) == bigramsText.end()) 
+            bigramsText[currentBigram] = 1;
+        else
+            bigramsText[currentBigram]++;
+    }
+    std::vector<std::pair<std::wstring, uint>> bigramsFile;
+    inFileUTF8_FORMAT_WCHAR_COUNT(bigramsFile, RUSSIAN_BIGRAMS);
+    std::unordered_map<std::wstring, uint> bigramsFileMap;
+    for (const auto &x : bigramsFile)
+        bigramsFileMap[x.first] = x.second;
+
+    std::wstring reservMonogramsKey = monogramsKey;
+    double _Point = dispersion(bigramsText, bigramsFileMap);
+    std::cout << _Point << std::endl;
+
+
+    for (size_t _Off = 0; _Off < COUNT_RUSSIAN_ABC - 1; _Off++) {
+        std::swap(monogramsKey[_Off], monogramsKey[_Off + 1]);
+
+        monogramsMap.clear();
+        for (size_t _Pos = 0; _Pos < monogramsKey.size(); _Pos++)
+            monogramsMap[monogramsKey[_Pos]] = monogramsFile[_Pos].first.at(0);
+
+        textDecrypted.clear();
+        for (auto &x : text) 
+            textDecrypted += monogramsMap[x];
+
+        bigramsText.clear();
+        for (int _Pos = 0; _Pos < textDecrypted.size() - 1; _Pos++) {
+            std::wstring currentBigram = textDecrypted.substr(_Pos, 2);
+
+            if(bigramsText.find(currentBigram) == bigramsText.end()) 
+                bigramsText[currentBigram] = 1;
+            else
+                bigramsText[currentBigram]++;
+        }
+
+        double current_Point = dispersion(bigramsText, bigramsFileMap);
+        std::cout << current_Point << std::endl;
+
+        if (_Point < current_Point)
+            std::swap(monogramsKey[_Off], monogramsKey[_Off + 1]);
+        else _Point = current_Point;
+    }
+
+    std::wcout << textDecrypted;
+
+    // for(const auto &x : bigramsFile)
+    //     std::wcout << x.first << " " << x.second << std::endl;
+}
+
+double Crypter::dispersion(std::unordered_map<std::wstring, uint> &_T, std::unordered_map<std::wstring, uint> &_F) {
+    double _Point = 0;
+    for (const auto &_Item : _T) 
+        if (_F.find(_Item.first) != _F.end())
+            _Point += pow(static_cast<double>(_Item.second) / _F[_Item.first], 2);     
+    
+    return _Point;
 }
 
 _CRYPTER_END
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
 #include <string>
+#include <filesystem>
 
 #include "Crypter.hpp"
 using namespace crypter;
 
 int main(int argc, char *argv[])
 {
-    Crypter text;
+    Crypter A;
+    // std::wstring key = A.generateRandomKey();
 
+    // std::wstring text = A.EncryptText(key, std::filesystem::path("texts/Jules_Verne.txt"));
+    // A.outFileUTF8(text, std::filesystem::path("texts/Jules_Verne_encrypted.txt"));
 
-    
+    A.FrequencyAnalysis(std::filesystem::path("texts/Jules_Verne_encrypted.txt"));
+
+    // std::wstring key = A.generateRandomKey();
+
+    // std::wstring key = inKeyFromFileUTF8(KEY);
+
+    // std::wstring text = A.EncryptText(key, COMPOSITION);
+    // A.outFileUTF8(text, COMPOSITION_ENCRYPTED);
+
+    // wcout_t(text);
+
+    // A.outFileUTF8(key, KEY);
+    // outFileUTF8(text, ENCRYPTED_ASSAY);
 }
